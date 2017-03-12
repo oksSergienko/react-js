8899ff7bb9929ccf3a557bff61c482efc76fd51e
O.Sergienko <oksana.sergienko@guidance.com>
2017-03-12 19:34:23 +0300

    comment form

--- src/components/CommentForm/SingleInput.js
+++ src/components/CommentForm/SingleInput.js
@@ -4,7 +4,7 @@ const SingleInput = (props) => (
 <div className="form-group">
     <label className="form-label">{props.title}</label>
     <input className="form-input" name={props.name} type={props.inputType} value={props.content}
-            onChange={props.controlFunc} placeholder={props.placeholder} />
+            onChange={props.controlFunc} placeholder={props.placeholder}  />
 </div>
 );
 
--- src/components/CommentForm/TextArea.js
+++ src/components/CommentForm/TextArea.js
@@ -5,7 +5,9 @@ const TextArea = (props) => (
 <div className="form-group">
     <label className="form-label">{props.title}</label>
     <textarea className="form-input" style={props.resize ? null : {resize: 'none'}} name={props.name} rows={props.rows} value={props.content}
-              onChange={props.controlFunc} placeholder={props.placeholder} />
+              onChange={props.controlFunc} placeholder={props.placeholder}
+
+    />
 </div>
 );
 
